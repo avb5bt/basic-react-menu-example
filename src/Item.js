f7411f139c8096158f7112138ad124dabbef5659
avb5bt <81479081+avb5bt@users.noreply.github.com>
2022-04-21 16:30:54 -0400

    Notes on Item.js (level 1 componrnt)

--- src/Item.js
+++ src/Item.js
@@ -7,3 +7,12 @@ function Item(props) {
 }
 
 export default Item;
+
+/*
+Notes:
+1) Note that the item, the most basic of components, has almost nothing in it.
+    a) it takes in a prop which is a way for the parent to pass to its children. Parent passes in information using attributes in html tag.
+    b) in the most basic of components (such as this), you don't use user-defined components. You use usually default components
+    c) props is an OBJECT encompassing everything in those html tags. Think of it like a map kinda. 
+    d) all it is displaying in the name of the food and the price. Rather than having a toString() method or converting to a string, just use ${JS expression} 
+*/
