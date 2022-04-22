bba424adbca84abd01ec57d55b1c17b3c40debf4
avb5bt <81479081+avb5bt@users.noreply.github.com>
2022-04-22 09:28:32 -0400

    Level 3 notes for component

--- src/Menu.js
+++ src/Menu.js
@@ -17,3 +17,19 @@ function Menu(props) {
 }
 
 export default Menu;
+/****************LEVEL 3: MENU******************
+What does it encaptulate: Meal (Level 2)
+
+What does it return: default components 
+
+What are the props: Reveice an object with 3d array (really an object)
+
+What are the states:  showOnlyVegetarian=>used only when we declare a button
+
+Other JS elements to mention:
+1) notice that the state is only declared when we have an event handler object (we have this event handler object in the parent class)
+
+2) The object is really a 3d object. Object.keys() turns this object into an array (breakfast, lunch, dinner). It passes on all those things: the mealName (breakfast,lunch, dinner) and the meals array
+
+3) it also passes the showOnlyVegetarian state. Notice that each item has a state that indicates this (notice how the parent and child at both ends have some information to use for state use)
+***********/
